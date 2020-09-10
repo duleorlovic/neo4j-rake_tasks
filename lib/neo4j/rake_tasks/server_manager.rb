b75b9ff957312fa8d6072208fbe51c2cc8711ba6
Dusan Orlovic <duleorlovic@gmail.com>
2020-09-10 05:49:56 +0200

    Increase version

--- lib/neo4j/rake_tasks/server_manager.rb
+++ lib/neo4j/rake_tasks/server_manager.rb
@@ -275,7 +275,7 @@ module Neo4j
 
       private
 
-      NEO4J_VERSIONS_URL = 'https://raw.githubusercontent.com/neo4jrb/neo4j-rake_tasks/master/neo4j_versions.yml'
+      NEO4J_VERSIONS_URL = 'https://raw.githubusercontent.com/duleorlovic/neo4j-rake_tasks/master/neo4j_versions.yml'
 
       def server_version_greater_than_or_equal_to?(version)
         Gem::Version.new(server_version) >= Gem::Version.new(version)
